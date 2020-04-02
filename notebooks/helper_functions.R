3ce6f567a2b7b198519834d35d9661fa51cbc126
Marcelo Eduardo Redoschi <marcelo@studio2m.com>
2020-04-02 12:09:02 +0200

    modified:   notebooks/helper_functions.R
      days_before_today function (takes a numeric argument)
    
    	modified:   notebooks/national.Rmd
    
      Calculated deltas (dt - previous_dt) for the pertinent columns.

--- notebooks/helper_functions.R
+++ notebooks/helper_functions.R
@@ -4,18 +4,21 @@
   
   }
 
-  yesterday <- function(){ 
-      ymd(format(Sys.time(), "%Y-%m-%d"))-1 
+  days_before_today <- function(n) { 
+      
+    ymd(today())-n 
     
+  }
+  
+  yesterday <- function(){ 
+      days_before_today(1)
   }
   
   two_days_ago  <- function(){ 
-    ymd(format(Sys.time(), "%Y-%m-%d"))-2
-    
+    days_before_today(2)
   }
   
   three_days_ago  <- function(){ 
-    ymd(format(Sys.time(), "%Y-%m-%d"))-3
-    
+    days_before_today(3)
   }
   
