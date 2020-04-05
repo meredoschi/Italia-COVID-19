d18242a1845b4855fa2c2c3ca83941a6a156a7b1
Marcelo Eduardo Redoschi <marcelo@studio2m.com>
2020-04-05 22:21:09 +0200

    Created cubic_rate_plot function
      To do: x-axis labels (add the correct and corresponding province abbreviations)
    
     	modified:   notebooks/helper_functions.R
    
     	modified:   notebooks/provinces_calc.Rmd
    
      Generated graphs programmatically (via for loop at present)
    
    	modified:   notebooks/provinces_calc.html

--- notebooks/helper_functions.R
+++ notebooks/helper_functions.R
@@ -33,3 +33,27 @@
     min(temp$dt)
   }
   
+  cubic_rate_plot <- function(df, specified_region, chosen_color){ 
+
+  
+    local_province_codes<-data.frame(df[grep(specified_region,df$region_name),]$province_code)
+    colnames(local_province_codes)<-c('province_code')
+
+    local_province_data<-inner_join(provinces_calc, local_province_codes, by='province_code')
+    
+    local_province_data<-arrange(local_province_data, province_name)    
+    most_recent_dt<-max(local_province_data$dt)
+    three_days_prior<-most_recent_dt-3 
+    
+    local_province_data$abbrev<-toupper(as.character(substring(local_province_data$province_abbreviation,1,2)))
+    local_province_data$c<-(local_province_data$cubic_rate-1)*100
+    local_province_data_key<-select(local_province_data,province_name, province_abbreviation )
+    
+    most_recent_dt<-max(local_province_data$dt)
+    three_days_prior<-most_recent_dt-3 
+  
+   plot(main=paste(specified_region,"(Italy)"), row_number(local_province_data$abbrev), df$c, xaxt='n', xlab='COVID-19 Epidemic (total case progression by province)', ylab="Average % rate (last three days - cubic root) ", sub=paste(format(three_days_prior,"%d %b"),"-",format(most_recent_dt,"%d %b %Y")), col=chosen_color)
+#  axis(1, row_number(local_province_data$province_abbreviation), local_province_data$province_abbreviation)
+    
+  }
+  
